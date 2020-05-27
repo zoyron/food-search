9fdc29b02e8526dfedcd1972de8a18065ce7f85e
zoyron <sgrarora3@gmail.com>
2020-05-27 14:28:20 +0530

    cleaned up public folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
 import Axios from "axios";
+import Recipe from "./components/Recipe";
 
 const App = () => {
   const [foodName, setFoodName] = useState("");
+  const [recipes, setRecipes] = useState([]);
   const APP_ID = "5bc39e25";
   const APP_KEY = "465925befdba4cf454f5040cdf81db73";
   const url = `https://api.edamam.com/search?q=${foodName}&app_id=${APP_ID}&app_key=${APP_KEY}`;
   const getData = async () => {
     console.clear();
     const response = await Axios.get(url);
-    for (
-      let i = 0;
-      i < response.data.hits[0].recipe.ingredientLines.length;
-      i++
-    )
-      console.log(response.data.hits[0].recipe.ingredientLines[i]);
-    // console.log(response);
+    setRecipes(response.data.hits);
+    console.log(recipes);
   };
   const onSubmit = (e) => {
     e.preventDefault();
@@ -40,6 +37,9 @@ const App = () => {
         />
         <input type="submit" value="search" />
       </form>
+      <div className="recipies">
+        {recipes !== [] && recipes.map((recipe) => <Recipe recipe={recipe} />)}
+      </div>
     </div>
   );
 };
