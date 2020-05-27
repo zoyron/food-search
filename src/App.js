fe8c46450ee22321abb99b578529b2de6d7c84ee
zoyron <sgrarora3@gmail.com>
2020-05-27 12:40:54 +0530

    added axios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 const App = () => {
   const APP_ID = "5bc39e25";
   const APP_KEY = "465925befdba4cf454f5040cdf81db73";
-  const url = `"https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"`;
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   return (
     <div className="App">
       <h1>Food Searching App</h1>
