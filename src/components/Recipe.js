34aa38c4e3c9bfba7c96a02c458b9b424df416aa
zoyron <sgrarora3@gmail.com>
2020-05-27 15:03:40 +0530

    fixed show button

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import RecipeDetails from "./RecipeDetails";
 
 const Recipe = ({ recipe }) => {
+  const [show, setShow] = useState(false);
+  const { label, image, url, ingredients } = recipe.recipe;
   return (
-    <div>
-      <h2> recipe </h2>
+    <div className="recipe">
+      <h2> {label} </h2>
+      <img src={image} alt={label} />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        URL
+      </a>
+      <button onClick={() => setShow(!show)}>Ingredients</button>
+      {show && <RecipeDetails ingredients={ingredients} />}
     </div>
   );
 };
