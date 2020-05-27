382991eed73d50e05149096c259ec3bdd1d63e6b
zoyron <sgrarora3@gmail.com>
2020-05-27 13:00:37 +0530

    console logged the recipie for pizza

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
+import Axios from "axios";
 
 const App = () => {
   const APP_ID = "5bc39e25";
   const APP_KEY = "465925befdba4cf454f5040cdf81db73";
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const url = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const getData = async () => {
+    const response = await Axios.get(url);
+    for (
+      let i = 0;
+      i < response.data.hits[0].recipe.ingredientLines.length;
+      i++
+    )
+      console.log(response.data.hits[0].recipe.ingredientLines[i]);
+    // console.log(response);
+  };
   return (
     <div className="App">
-      <h1>Food Searching App</h1>
+      <h1 onClick={getData}>Food Searching App</h1>
     </div>
   );
 };
